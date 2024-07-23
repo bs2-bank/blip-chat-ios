354ae0b91d0078161b21a95f5d190ce3e4f65ccc
Vitor Zitti <vitorzitti@MACSPO0001.local>
2024-07-23 09:36:30 -0300

    try

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BlipChat",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "BlipChat",
+            targets: ["BlipChat"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "BlipChat"),
+        .testTarget(
+            name: "BlipChatTests",
+            dependencies: ["BlipChat"]),
+    ]
+)
